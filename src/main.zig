3d9e769b00345ea2812f913918d42e4a68ac5eb1
Alex Miller <millerdevel@gmail.com>
2022-12-17 16:11:04 -0800

    String example works!

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,37 @@
-// we'll import this from JS-land
-extern fn console_log_ex(message: [*]const u8, length: u8) void;
+const std = @import("std");
 
-// we'll export this to JS-land
-export fn add(a: i32, b: i32) i32 {
-    const log = "happy happy joy joy";
-    console_log_ex(log, log.len);
-    return a + b;
+var allocator: std.mem.Allocator = undefined;
+
+// Initialize any starting state.  This is automatically called after the
+// WASM is loaded by the browser.
+//
+// The allocator could be assigned at global scope, but this is an example.
+export fn start() void {
+    // Implemented by std.heap.WasmPageAllocator on wasm32.
+    allocator = std.heap.page_allocator;
+}
+
+// For JS to give Zig a string, the string must be in WASM memory.  It's not
+// safe to just write anywhere, as it doesn't know what's allocated and free.
+// Thus, we expose a way for JS to get an arbitrary block of memory to use to
+// pass to Zig.
+export fn alloc(size: u32) [*]const u8 {
+    const slice = allocator.alloc(u8, size) catch @panic("alloc: allocator.alloc");
+    return slice.ptr;
+}
+
+// An arbitrary computation, which returns a newly allocated string.
+export fn double(ptr: [*]u8, size: u32) [*:0]u8 {
+    var in_slice: []u8 = ptr[0..size];
+    var out_str: []u8 = allocator.alloc(u8, size * 2 + 1) catch @panic("double: allocator.alloc");
+    std.mem.copy(u8, out_str[0..size], in_slice);
+    std.mem.copy(u8, out_str[size .. size * 2], in_slice);
+    out_str[size * 2] = 0;
+    return out_str[0 .. out_str.len - 1 :0].ptr;
+}
+
+// The string returned by `double` is owned by `allocator`, and thus JS will
+// need to call into WASM to have it freed by `allocator` as well.
+export fn free(ptr: [*]u8, size: u32) void {
+    allocator.free(ptr[0..size]);
 }
--- www/wasmtest.js
+++ www/wasmtest.js
@@ -1,25 +1,62 @@
 // just keep a global ref to the instance around for convenience
 var instance;
-// this function will be imported for wasm to use
-function console_log_ex(location, size) {
-    var buffer = new Uint8Array(instance.exports.memory.buffer, location, size);
-    var decoder = new TextDecoder();
-    var string = decoder.decode(buffer);
-    console.log(string);
-}
+
+function encodeString(instance, string) {
+  const buffer = new TextEncoder().encode(string);
+  // `double` takes a [*]u8, so no need for length+1 for a null byte.
+  const pointer = instance.exports.alloc(buffer.length);
+  const slice = new Uint8Array(
+    instance.exports.memory.buffer,
+    pointer,
+    buffer.length
+  );
+  slice.set(buffer);
+  return [pointer, buffer.length];
+};
+
+function decodeString(instance, ptrZ) {
+  const dv = new DataView(instance.exports.memory.buffer);
+  var length = 0;
+  while (dv.getUint8(ptrZ + length)) {
+    length += 1;
+  }
+
+  const slice = new Uint8Array(
+    instance.exports.memory.buffer,
+    ptrZ,
+    length
+  );
+  return new TextDecoder().decode(slice);
+};
+
 // define our imports
 var imports = {
     env: {
-        console_log_ex: console_log_ex
     }
 };
+
 // do the thing
 fetch("wasmtest.wasm")
     .then(function (response) { return response.arrayBuffer(); })
     .then(function (bytes) { return WebAssembly.instantiate(bytes, imports); })
     .then(function (results) {
     instance = results.instance;
-    // grab our exported function from wasm
-    var add = results.instance.exports.add;
-    console.log(add(3, 4));
+
+    // Docs say that this should be automatically called, but that appears
+    // to not be true.
+    instance.exports.start();
+
+    // Fetch a JS string, and convert it into a slice for zig.
+    let input_text = document.getElementById("input").textContent;
+    let [in_ptr, in_len] = encodeString(instance, input_text);
+
+    // Invoke the function, and get a [*:0]u8 in return.
+    let out_ptr = instance.exports.double(in_ptr, in_len);
+
+    // Convert the [*:0]u8 to a JS string, and then free it from zig's memory.
+    let out_str = decodeString(instance, out_ptr);
+    instance.exports.free(out_ptr, out_str.length);
+
+    // Put the output back into the document.
+    document.getElementById("output").textContent = out_str;
 });
